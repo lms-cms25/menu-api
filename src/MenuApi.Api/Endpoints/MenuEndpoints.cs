79e2ed585f68a1e634b1169acad2128b1fd4d1cd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-05-02 20:21:50 +0200

    basic configuration and extensions applied

---
+++ src/MenuApi.Api/Endpoints/MenuEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace MenuApi.Api.Endpoints;
+
+public static class MenuEndpoints
+{
+    public static void MapMenuEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/api/menu").WithTags("Menu").WithDescription("Side bar menu");
+
+
+    }
+}
---
+++ src/MenuApi.Api/OpenApi/OpenApiConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+
+namespace MenuApi.Api.OpenApi;
+
+public static class OpenApiConfiguration
+{
+    public static IServiceCollection AddOpenApiConfiguration(this IServiceCollection services)
+    {
+        services.AddOpenApi(options =>
+        {
+            options.AddDocumentTransformer<OpenApiDocumentTransformer>();
+        });
+
+        return services;
+    }
+}
