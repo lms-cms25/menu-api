ee1809707be6fe0e1683dab0be94645979cd887d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-05-02 22:22:47 +0200

    add get menu method - no implementation yet

--- src/MenuApi.Api/Endpoints/MenuEndpoints.cs
+++ src/MenuApi.Api/Endpoints/MenuEndpoints.cs
@@ -6,6 +6,12 @@ public static class MenuEndpoints
     {
         var group = app.MapGroup("/api/menu").WithTags("Menu").WithDescription("Side bar menu");
 
+        group.MapGet("/get", GetMenu);
 
+    }
+    public static async Task<IResult> GetMenu()
+    {
+
+        return Results.Ok();
     }
 }
